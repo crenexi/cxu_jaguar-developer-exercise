70a4842853795aed7d4f1daf06d6a3fb014c6d4a
James Blume <james@crenexi.com>
2023-04-12 18:16:38 -0700

    Fix making helper methods to class

--- exercise.rb
+++ exercise.rb
@@ -3,7 +3,9 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
 
-  def is_capitalized(str) str.capitalize() == str end
+  def self.is_capitalized(str)
+    str.capitalize() == str
+  end
 
   def self.marklar(str)
     regexMin5Char = /\b\w{5,}\b/
@@ -14,7 +16,7 @@ class Exercise
 
   # Return the sum of all even numbers in the Fibonacci sequence up to nth
 
-  def sum_evens(arr)
+  def self.sum_evens(arr)
     sum_of_evens = 0
     arr.each do |n|
       if n.even?
