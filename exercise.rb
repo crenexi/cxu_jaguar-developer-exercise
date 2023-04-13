3402f0593590e4c229b0e273cb8761e11abb9c08
James Blume <james@crenexi.com>
2023-04-12 17:42:09 -0700

    Implemented marklar method

--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,15 @@
 class Exercise
-
   # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
+
+  def isCapitalized(str) str.capitalize() == str end
+
   def self.marklar(str)
-    # TODO: Implement this method
+    regexMin5Char = /\b\w{5,}\b/
+    return str.gsub(regexMin5Char) {
+      |match| isCapitalized(match) ? "Marklar" : "marklar"
+    }
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
